e886489aee6866bcbbe1ff49023c2ed43de222fa
Dayane David <dayanecrisdavid@gmail.com>
2021-08-31 10:20:08 -0300

    Fazendo com que contatos sejam levantados a partir de um serviço

--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContatoService } from 'src/app/services/contato.service';
 import { Contato } from '../models/Contato';
 
 @Component({
@@ -8,56 +9,13 @@ import { Contato } from '../models/Contato';
 })
 export class ListaDeContatosComponent implements OnInit {
 
-  contatos:Contato[] = [
+  public contatos:Contato[];
+  private cs:ContatoService = new ContatoService();
 
-    { 
-      nome:"Denis",
-      email:"Denis.pereira@gmail.com",
-      telefones: ['1111-1111']
-    },
-    { 
-      nome:"Denis 2",
-      email:"Denis.pereira@gmail.com",
-      telefones: ['2222 - 2222']
-    },
-    { 
-      nome:"Denis 3",
-      email:"Denis.pereira@gmail.com",
-      telefones: ['3333 - 3333']
-    },
-    { 
-      nome:"Denis 4",
-      email:"Denis.pereira@gmail.com",
-      telefones: ['4444 - 4444']
-    },
-    { 
-      nome:"Denis 5",
-      email:"Denis.pereira@gmail.com",
-      telefones: ['5555 - 5555']
-    },
-    { 
-      nome:"Denis 6",
-      email:"Denis.pereira@gmail.com",
-      telefones: ['6666 - 6666']
-    },
-    { 
-      nome:"Denis 7",
-      email:"Denis.pereira@gmail.com",
-      telefones: ['7777 - 7777']
-    },
-    { 
-      nome:"Denis 8",
-      email:"Denis.pereira@gmail.com",
-      telefones: ['8888 - 8888']
-    },
-    { 
-      nome:"Denis 9",
-      email:"Denis.pereira@gmail.com",
-      telefones: ['9999 - 9999']
-    },
-  ]
-
-  constructor() { }
+  constructor() { 
+    this.contatos = this.cs.getContatos();
+  }
+    
 
   ngOnInit(): void {
   }
